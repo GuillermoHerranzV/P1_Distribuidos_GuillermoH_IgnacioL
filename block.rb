d22bb76429af26a61375841ffca04d4cf814f14f
GuillermoHerranzV <149266597+GuillermoHerranzV@users.noreply.github.com>
2025-03-16 15:42:33 +0100

    Add files via upload

--- block.rb
+++ block.rb
@@ -1,23 +1,24 @@
 class Block
-  attr_reader :index, :timestamp, :transactions, 
-							:transactions_count, :previous_hash, 
-							:nonce, :hash 
+  attr_reader :index, :timestamp, :data, :previous_hash, :nonce, :hash, :creator 
 
-  def initialize(index, transactions, previous_hash)
-    @index         		 	 = index
-    @timestamp      	 	 = Time.now
-    @transactions 	 		 = transactions
-		@transactions_count  = transactions.size
-    @previous_hash 		 	 = previous_hash
-    @nonce, @hash  		 	 = compute_hash_with_proof_of_work
+  def initialize(index, timestamp, data, previous_hash, creator)
+    @index = index
+    @timestamp = timestamp
+    @data = data
+    @previous_hash = previous_hash
+    @creator = creator
+    @nonce = 0
+    @hash = compute_hash_with_proof_of_work
   end
+  
 
-	def compute_hash_with_proof_of_work(difficulty="00")
+	def compute_hash_with_proof_of_work(difficulty="0000")
 		nonce = 0
 		loop do 
 			hash = calc_hash_with_nonce(nonce)
 			if hash.start_with?(difficulty)
-				return [nonce, hash]
+        #modificado el retorno para que el hash no sea un array y no de error
+				return hash
 			else
 				nonce +=1
 			end
@@ -37,10 +38,11 @@ class Block
 
   def self.first( *transactions )    # Create genesis block
     ## Uses index zero (0) and arbitrary previous_hash ("0")
-    Block.new( 0, transactions, "0" )
+    Block.new(0, Time.now, "Genesis Block", "0", "Elam")
   end
 
   def self.next( previous, transactions )
-    Block.new( previous.index+1, transactions, previous.hash )
+    # Modificado para que funcione con los nuevos atributos del bloque
+    Block.new( previous.index+1,Time.now, transactions, previous.hash, "Elam" )
   end
 end  # class Block
